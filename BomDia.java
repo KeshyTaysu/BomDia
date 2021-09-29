97b477a88e00d9ad9532ea2e777219b9cc95568d
KeshyTaysu <eshileyestudos@gmail.com>
2021-09-29 17:19:12 -0300

    oi

---
+++ BomDia.java
@@ -0,0 +1,49 @@
+package bomdia;
+
+import java.util.Scanner;
+
+public class BomDia {
+
+    
+    
+    public static void main(String[] args) {
+     
+          Scanner input = new Scanner(System.in);
+                   
+                   String noite = input.nextLine();
+          
+                             switch (noite) {
+                                 
+                                      case "M" :
+                                          
+                                                System.out.println("Bom dia!");
+                                       
+                                                break;
+                                                
+                                      case "V" :
+                                                System.out.println("Bom tarde!");
+                                                
+                                                break;
+                                                
+                                      case "N" :
+                                                 System.out.println("Bom noite!");          
+                                                 
+                                                break;
+                                                 
+                  // boolean dia = input.nextBoolean();
+          
+                             //if (dia = true) {
+                                 //      System.out.println("Olá! Bom dia!");
+  }
+                   
+                        //     else {
+                                   //   System.out.println("Olá! Boa noite!");
+  }
+          
+              
+        
+     
+        
+ }
+    
+}
